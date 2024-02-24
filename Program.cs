fde18e541b2d0541dc6315f9c3d7aad84613d304
aleosorio22 <alejandroosorio022@gmail.com>
2024-02-24 11:10:46 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿
+//ENUNCIADO1
+//for (int i = 2; i <=10; i += 2)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+//ENUNCIADO2 
+//int n = 0;
+//int suma = 0;
+
+//while (n >= 0)
+//{
+//    Console.WriteLine("escriba el numero positivo entero");
+//    int.TryParse(Console.ReadLine(), out n);
+//    suma = suma += n;
+//}
+//Console.WriteLine($"Suma: {suma}");
+
+
+//ENUNCIADO3
+//string contra = "";
+//do
+//{
+//    Console.WriteLine("ingrese la contrasena");
+//    contra = Console.ReadLine();
+//}
+//while (contra != "contrasena");
+//if (contra == "contrasena")
+//{
+//    Console.WriteLine("Welcome");
+//}
+
+
+
+
+
+
+//ESTOS SON LOS EJEMPLOS DE LA CLASE 4
+
+//int x = 1;
+
+//while(x <= 500)
+//{
+//    Console.WriteLine(x++);
+//}
+
+//int x1 = 50;
+
+//while (x1 <= 100)
+//{
+//    Console.WriteLine(x++);
+//}
+
+//int x = -50;
+
+//while (x <= 0)
+//{
+//    Console.WriteLine(x++);
+//}
+
+//int x = 2;
+
+//while (x <= 100)
+//{
+//    Console.WriteLine(x = x+2);
+//}
+
+
+//int n1 = 0;
+//int mayor = 0;
+
+//do
+//{
+//    Console.WriteLine("ingrese numero");
+//    int.TryParse(Console.ReadLine(), out n1 );
+//}
+//while (n1 !=0);
+//if  (n1 > mayor)
+//{
+//    mayor = n1;
+//}
+//Console.WriteLine(mayor);
+
+
+
+//for (int h = 1; h < 24; h++)
+//{
+//    for (int m = 1; m < 60; m++)
+//    {
+//        for (int s = 1; s < 60; s++)
+//        {
+//            Console.SetCursorPosition(10, 10);
+//            Console.WriteLine($"{h}:{m}:{s}");
+//            Thread.Sleep(1000);
+//        }
+//    }
+//}
+
